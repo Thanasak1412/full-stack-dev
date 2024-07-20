2b74d12292711166bb3e1cc38ddc3d12f8d3bdfa
Thanasak Srisaeng <Thanasak@Thanasak1412.local>
2024-07-20 17:09:16 +0700

    Init project

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+import { TransformInterceptor } from './transform/transform.interceptor';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const logger = new Logger();
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>('app.port');
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  await app.listen(port);
+
+  logger.log(`Application is running on port: ${port}`);
+}
+bootstrap();
